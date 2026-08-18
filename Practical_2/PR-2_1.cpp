78df5e47b9743d309b0b8a7a58bbd7b24c55d2bb
Ansh26 <anshdabhi26@gmail.com>
2026-08-18 15:51:46 +0530

    Update PR-2_1.cpp

--- Practical_2/PR-2_1.cpp
+++ Practical_2/PR-2_1.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
 using namespace std;
+int iterativeSearch(string arr[], int n, string target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+
+int recursiveSearch(string arr[], int n, string target, int index)
+{
+    if (index == n)
+        return -1;
+
+    if (arr[index] == target)
+        return index;
+
+    return recursiveSearch(arr, n, target, index + 1);
+}
 
 int main()
 {
-    string plates[] = {"GJ01AB1111", "GJ02CD2222", "GJ03EF3333", "GJ04GH4444"};
-    int n = 4;
-    string target = "GJ03EF3333";
+    int n;
 
-    int result = 0;
+    cout << "Enter number of vehicles: ";
+    cin >> n;
 
+    string plates[100];
+
+    cout << "Enter license plates:";
     for (int i = 0; i < n; i++)
     {
-        if (plates[i] == target)
-        {
-            result = i + 1;
-            break;
-        }
+        cin >> plates[i];
     }
 
-    if (result != 0)
-    {
-        cout << "Target plate found at position : " << result << endl;
-    }
+    string target;
+
+    cout << "Enter target license plate: ";
+    cin >> target;
+
+   
+    int pos1 = iterativeSearch(plates, n, target);
+
+    if (pos1 != -1)
+        cout << "Iterative Search: Found at position " << pos1 + 1 << endl;
     else
-    {
-        cout << "Target plate not found." << endl;
-    }
+        cout << "Iterative Search: Not Found" << endl;
+
+    int pos2 = recursiveSearch(plates, n, target, 0);
+
+    if (pos2 != -1)
+        cout << "Recursive Search: Found at position " << pos2 + 1 << endl;
+    else
+        cout << "Recursive Search: Not Found" << endl;
 
     return 0;
 }
